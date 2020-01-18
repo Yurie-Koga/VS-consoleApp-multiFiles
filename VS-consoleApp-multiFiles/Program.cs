6f6e67c4f0057b098f72b0897c23a021bb36ab33
Yurie <yurie.koga.a@gmail.com>
2020-01-17 20:00:03 -0800

    Add FibonacciGenerator

--- VS-consoleApp-multiFiles/Program.cs
+++ VS-consoleApp-multiFiles/Program.cs
@@ -6,7 +6,46 @@ namespace VS_consoleApp_multiFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var generator = new FibonacciGenerator();
+            foreach (var digit in generator.Generate(15))
+            {
+                Console.WriteLine(digit);
+            }
+
+
+
+            //    if (args.Length > 0)
+            //    {
+            //        Console.WriteLine($"Hello {args[0]}!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Hello!");
+            //    }
+
+            //    Console.WriteLine("Fibonacci Numbers 1-15:");
+
+            //    for (int i = 0; i < 15; i++)
+            //    {
+            //        Console.WriteLine($"{i + 1}: {FibonacciNumber(i)}");
+            //    }
+            //}
+
+            //static int FibonacciNumber(int n)
+            //{
+            //    int a = 0;
+            //    int b = 1;
+            //    int tmp;
+
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        tmp = a;
+            //        a = b;
+            //        b += tmp;
+            //    }
+
+            //    return a;
         }
+
     }
 }
